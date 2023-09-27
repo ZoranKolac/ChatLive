f2449d1a65f819c560fbff7d1bf9dbdaf818288e
Zoran Kolac <kizo22@gmail.com>
2023-09-27 21:07:32 +0200

    Create Chat App

---
+++ src/pages/_document.js
@@ -0,0 +1,13 @@
+import { Html, Head, Main } from 'react/document'
+
+export default function Document() {
+  return (
+    <Html lang='en'>
+      <Head />
+      <body>
+        <Main />
+        
+      </body>
+    </Html>
+  )
+}
